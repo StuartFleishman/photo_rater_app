807b4de822773242bdeb5e0427a2cdb5911c2bf1
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-16 12:34:29 -0400

    added user model/table as well as bcrypt for password and omniauth

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+
+  has_secure_password 
+
+  def self.create_from_omniauth(auth)
+    User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u| 
+      u.email = auth['info']['email']
+      u.name = auth['info']['name']
+      u.password = SecureRandom.hex(16)
+    end
+  end
+end
