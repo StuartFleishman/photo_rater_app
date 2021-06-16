00fc4bfa888c87350f00df99a559e24764553d42
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-16 13:20:32 -0400

    addef fast json api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  validates :name, presence: true
-  validates :email, uniqueness: true, presence: true
+  validates :user_name, presence: true
+  # validates :email, uniqueness: true, presence: true
 
   has_secure_password 
 
